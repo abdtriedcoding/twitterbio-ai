7a7de31cde32201a29e550b540df51ba84496ba6
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-04-23 13:39:56 +0530

    add rate limiting

--- app/api/completion/route.ts
+++ app/api/completion/route.ts
@@ -1,4 +1,6 @@
+import { Redis } from "@upstash/redis";
 import Anthropic from "@anthropic-ai/sdk";
+import { Ratelimit } from "@upstash/ratelimit";
 import { AnthropicStream, StreamingTextResponse } from "ai";
 
 // Create an Anthropic API client (that's edge friendly)
@@ -6,12 +8,34 @@ const anthropic = new Anthropic({
   apiKey: process.env.ANTHROPIC_API_KEY || "",
 });
 
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+});
+
+// Create a new ratelimiter, that allows 2 requests per 30 minutes
+const ratelimit = new Ratelimit({
+  redis: redis,
+  limiter: Ratelimit.fixedWindow(2, "30 m"),
+});
+
 // IMPORTANT! Set the runtime to edge
 export const runtime = "edge";
 
 export async function POST(req: Request) {
+  if (ratelimit) {
+    const ip = req.headers.get("x-real-ip") ?? "local";
+    const rl = await ratelimit.limit(ip);
+
+    if (!rl.success) {
+      return new Response("Rate limit exceeded", { status: 429 });
+    }
+  }
+
   // Extract the `prompt` from the body of the request
   const { prompt } = await req.json();
+
+  if (!prompt) return new Response("Prompt is required", { status: 400 });
 
   // Ask Claude for a streaming chat completion given the prompt
   const response = await anthropic.messages.create({
